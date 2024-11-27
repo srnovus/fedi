679ceb4b75903ab46415867baed57265ab53f74f
Javier Caceres <novus@Javiers-MacBook-Pro.local>
2024-11-26 19:31:07 -0600

    beta

--- packages/frontend/src/ui/_common_/common.ts
+++ packages/frontend/src/ui/_common_/common.ts
@@ -151,7 +151,7 @@ export function openInstanceMenu(ev: MouseEvent) {
 	menuItems.push({
 		type: 'link',
 		text: i18n.ts.aboutMisskey,
-		to: '/about-misskey',
+		to: '/about-fedired',
 	});
 
 	os.popupMenu(menuItems, ev.currentTarget ?? ev.target, {
